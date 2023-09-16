985699842e0b15169e375abe54290f231e12eabc
Devam Gosalia <devamgosalia@gmail.com>
2023-09-16 22:20:47 +0530

    Project structure

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='datascienceproject',
 version='0.0.1',
 author='devam',
 author_email='devamgosalia@gmail.com',
